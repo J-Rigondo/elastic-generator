1d6ccf3382e6af8605616ea82144c9856fe53b86
Jun Hyeong Park <futuregoing46@gmail.com>
2023-12-01 13:30:45 +0900

    key gen

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 const csvReadBulkInsert = require("./libs/csv-reader");
 const { Client } = require("@elastic/elasticsearch");
-const { bulkInsert } = require("./utils");
+const { bulkInsert, generateRandomKey } = require("./utils");
 const { adList } = require("./data");
 
 async function main() {
-  const client = new Client({
-    node: "http://localhost:9200",
-  });
+  // const client = new Client({
+  //   node: "http://localhost:9200",
+  // });
+  //
+  // client.diagnostic.on("response", (err, result) => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     console.info(result);
+  //   }
+  // });
 
-  client.diagnostic.on("response", (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.info(result);
-    }
-  });
-
-  csvReadBulkInsert(client);
+  // csvReadBulkInsert(client);
 
   // await bulkInsert({client, indexName:'ad-index',dataset:adList});
+
+  console.log(generateRandomKey(32));
 }
 
 main();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,26 @@
-async function bulkInsert({client, indexName, dataset}) {
-    const operations = dataset.flatMap(doc => [{ index: { _index: indexName } }, doc]);
+const crypto = require("crypto");
+async function bulkInsert({ client, indexName, dataset }) {
+  const operations = dataset.flatMap((doc) => [
+    { index: { _index: indexName } },
+    doc,
+  ]);
 
-    const bulkResponse = await client.bulk({ refresh: true, operations });
+  const bulkResponse = await client.bulk({ refresh: true, operations });
 
-    console.log(bulkResponse);
+  console.log(bulkResponse);
 }
 
-module.exports = {bulkInsert}
+function generateRandomKey(length) {
+  const specialSymbol = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    const randomByte = crypto.randomBytes(1)[0];
+    result += characters[randomByte % charactersLength];
+  }
+  return result;
+}
+
+module.exports = { bulkInsert, generateRandomKey };
